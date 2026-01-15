f93dd6e821182cd197801e0ff148e6f31313e2e0
evgeny pekerman <geka.kud@gmail.com>
2026-01-15 17:07:18 +0200

    rename app to VeloMap and add more routes for debug

--- mobile/client/velo_map_app/lib/features/routes/data/datasources/routes_local_datasource.dart
+++ mobile/client/velo_map_app/lib/features/routes/data/datasources/routes_local_datasource.dart
@@ -29,7 +29,7 @@ class RouteLocalDatasource {
   }
 
   Future<List<String>> _routeFiles() async {
-    const allowedRoutes = {6, 9, 14};
+    const allowedRoutes = {6, 9, 14, 1, 4};
 
     final manifest1 = await AssetManifest.loadFromAssetBundle(rootBundle);
     final assets = manifest1.listAssets();
